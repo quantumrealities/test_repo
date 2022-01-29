7f0e2a3fbec58bdf1e15f4859299bf1c24d3b452
quantumrealities <93021207+quantumrealities@users.noreply.github.com>
2022-01-28 22:34:51 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,3 @@
-print("hello world!")
+local module = require(loadstring(game:HttpGet("https://raw.githubusercontent.com/quantumrealities/test_repo/main/module.lua?token=GHSAT0AAAAAABRBSPIV52BRXLGDMNIFR3JEYPUYPZQ"))())
+
+module:Test()
